7003e762d3fc207bdf3d536f388f51d7bbc4a5f2
ville <lilinitsy@gmail.com>
2018-12-02 15:46:29 -0600

    HSV conversion

--- src/MeanShiftVector.py
+++ src/MeanShiftVector.py
@@ -110,7 +110,7 @@ def create_particles(bounding_box: BoundingBox, num_windows: int) -> List[Partic
 
 
 def sliding_window_histograms(bounding_box: BoundingBox, particles: Particles) -> (np.ndarray, List):
-	histograms = [[0 for i in range(0, 3)] for j in range(0, len(particles))]
+	histograms = []
 	bounding_boxes = []
 
 	for i in range(0, len(particles)):
@@ -128,44 +128,31 @@ def sliding_window_histograms(bounding_box: BoundingBox, particles: Particles) -
 
 
 		mask[int(min_x):int(max_x), int(min_y):int(max_y)] = 255
-		
-		#histograms[i].append(histogram)
-		colours = ('b', 'g', 'r')
-		for j, cols in enumerate(colours):
-			histograms[i][j] = cv2.calcHist([image], [j], mask, [16], [0, 256])
+		histogram = cv2.calcHist([image], [0], mask, [16], [0, 256])
+		histograms.append(histogram)
 	return (histograms, bounding_boxes)
 
 # shape: rows, columns, channels (rgb)
 # 3-2: Compute colour histogram for each particle
-def generate_histograms(bounding_box: BoundingBox) -> np.ndarray:
-	histograms = []	
+def generate_histogram(bounding_box: BoundingBox) -> np.ndarray:
 	min_x = bounding_box.center_x - bounding_box.width / 2
 	max_x = bounding_box.center_x + bounding_box.width / 2
 	min_y = bounding_box.center_y - bounding_box.height / 2
 	max_y = bounding_box.center_y + bounding_box.height / 2
-	print("minx: ", min_x)
-	print("maxx: ", max_x)
-	print("miny: ", min_y)
-	print("maxy: ", max_y)
-	print("\n")
 	
 	# make a mask and get histogram in this window
 	mask = np.zeros(image.shape[:2], np.uint8)
 	mask[int(min_x):int(max_x), int(min_y):int(max_y)] = 255
-
-	colours = ('b', 'g', 'r')
-	for i, cols in enumerate(colours):
-		histogram = cv2.calcHist([image], [i], mask, [256], [0, 256])
-		histograms.append(histogram)
+	histogram = cv2.calcHist([image], [0], mask, [16], [0, 256])
 
 	# numpy.ndarray	
-	return histograms
+	return histogram
 
 
 # Not going to type annotate this yet
 def create_kernel(bounding_box):
-	x = bounding_box.width
-	y = bounding_box.height
+	x = int(bounding_box.width)
+	y = int(bounding_box.height)
 	kernel = [[0 for i in range(y)] for j in range(x)]
 
 	for i in range(int(x / 2)):
@@ -193,6 +180,12 @@ def create_kernel(bounding_box):
 # 3-3: Similiarity step, use Bhattacharyya Distance for PF
 # But for MSV, use the colour histogram similarity
 
+# From mpatacchiola - github user
+def histogram_similarity(hist1, hist2):
+	mininum = np.minimum(hist1, hist2)
+	intersection = np.true_divide(np.sum(mininum), np.sum(hist2))
+	print("intersection: ", intersection)
+	return intersection
 
 
 def draw_bounding_box(bounding_box: BoundingBox, image, color: (int, int, int)) -> None:
@@ -211,7 +204,16 @@ def tracking_histogram_routine(target_histogram, bounding_box: BoundingBox, kern
 	# Set that as the center, check if distance from current_center to the best window_histogram is below a distance
 	# If it isn't, make a new bounding box using window_histogram as the center,
 	# Recurse with target_histogram, new_bounding_box, kernel, new_window_histograms, new_window_bbs, window_kernels, new_center
-	''''''
+	maximum = 0
+	index = 0
+	for i in range(0, len(window_histograms)):
+		intersection = histogram_similarity(target_histogram, window_histograms[i])
+		if intersection > maximum:
+			max = intersection
+			maximum = i
+	print("max: ", maximum)
+	print("index: ", index)
+		
 
 
 def captureVideo(src) -> None:
@@ -244,7 +246,7 @@ def captureVideo(src) -> None:
 	kernel = create_kernel(bounding_box)
 
 
-	target_histograms = generate_histograms(bounding_box)
+	target_histogram = generate_histogram(bounding_box)
 	#tracking_histogram = np.zeros()
 	window_histograms = []
 	window_bbs = []
@@ -252,6 +254,7 @@ def captureVideo(src) -> None:
 	while(True):
 		# Capture frame-by-frame
 		ret, image = cap.read()
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 		if ret == False:
 			break
 		
@@ -268,9 +271,9 @@ def captureVideo(src) -> None:
 				window_kernels.append(window_kernel)
 
 
-			target_histograms = generate_histograms(bounding_box)
+			target_histograms = generate_histogram(bounding_box)
 			#tracking_histogram = tracking_histogram_routine(target_histogram, bounding_box, kernel)
-
+			tracking_histogram_routine(target_histogram, bounding_box, kernel, window_histograms, window_bbs, window_kernels, (bounding_box.center_x, bounding_box.center_y))
 
 
 			draw_bounding_box(bounding_box, image, color = (0, 0, 255))
